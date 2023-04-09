84ebadd65bf5f189fb0841b024dde2edfb3ac832
gopoma <gordono@unsa.edu.pe>
2023-04-08 21:34:35 -0500

    feat: add 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.gopomaagosto2022.cf",
-  integrations: [mdx()]
+	site: "https://www.gopomaagosto2022.cf",
+	integrations: [
+		mdx(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp"
+		})
+	]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,7 @@
+// eslint-disable-next-line
 /// <reference path="../.astro/types.d.ts" />
 // eslint-disable-next-line
 /// <reference types="astro/client" />
+// eslint-disable-next-line
+/// <reference types="@astrojs/image/client" />
+
